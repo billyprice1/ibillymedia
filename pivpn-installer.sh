14ff3a1469712de6eb799e9eac3e3881de46795a
billyprice1 <lkk29898@email.vccs.edu>
2017-08-28 19:37:08 +0100

    Update and rename pivpn.sh to pivpn-installer.sh

--- pivpn-installer.sh
+++ pivpn-installer.sh
@@ -1,5 +1,6 @@
 #/bin/bash
+apt-get update 
 apt-get install Dialog
 apt-get install curl 
 curl -L https://install.pivpn.io | bash
- echo nameserver 8.8.8.8 > /etc/resolv.conf.head      
+#Openvz VPS Only echo nameserver 8.8.8.8 > /etc/resolv.conf.head      
